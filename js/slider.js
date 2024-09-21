71623a242ed96211979be547b8d3671e985a3d2d
venteoo22 <sergio.venteo2222@gmail.com>
2024-09-21 18:36:22 +0200

    code

---
+++ js/slider.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+    let value;
+
+    if (buttonNext && buttonBefore) {
+        buttonNext.addEventListener('click', () => {
+            changePosition(1);
+        });
+
+        buttonBefore.addEventListener('click', () => {
+            changePosition(-1);
+        });
+    } else {
+        console.error('Botones no encontrados en el DOM');
+    }
+
+    const changePosition = (add) => {
+        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+        value = Number(currentTestimony);
+        value += add;
+
+        sliders[Number(currentTestimony) - 1].classList.remove('testimony__body--show');
+        if (value === sliders.length + 1 || value === 0) {
+            value = value === 0 ? sliders.length : 1;
+        }
+
+        console.log(value);
+
+        sliders[value - 1].classList.add('testimony__body--show');
+    }
+});
+
+
